cf9e1e2741aac016ffc7c2ce5e659d1f7f6c4019
Paul Worrall <p.r.worrall@gmail.com>
2025-11-18 10:36:14 +0000

    Try again to make imports easier

--- src/paye/__init__.py
+++ src/paye/__init__.py
@@ -1,1 +1,13 @@
+<<<<<<< HEAD
 from paye import *
+=======
+from .paye import (
+    Payslip,
+    TaxCode,
+    str_to_decimal,
+    PROGNOSTICATOR_ID,
+    SCOPES,
+    tax_due,
+    uk_tax_period_start_date,
+)
+>>>>>>> b4ba536 (Try again to make imports easier)
