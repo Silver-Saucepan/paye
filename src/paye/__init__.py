a30f3e3d8f84ea5fd8e05427e942174063ed4645
Paul Worrall <p.r.worrall@gmail.com>
2026-07-05 19:21:36 +0100

    Remove the str_to_decimal function
    It's not used within this package, let users define the functions
    they need

--- src/paye/__init__.py
+++ src/paye/__init__.py
@@ -2,7 +2,6 @@ from paye.paye import (
     DescribedAmount,
     Payslip,
     TaxCode,
-    str_to_decimal,
 )
 
-__all__ = ["DescribedAmount", "Payslip", "TaxCode", "str_to_decimal"]
+__all__ = ["DescribedAmount", "Payslip", "TaxCode"]
